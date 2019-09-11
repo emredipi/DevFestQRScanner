dc00b2a356487f246cfcad5ebca0cf764d977af2
Emre Dipi <mail@emredipi.com>
2019-09-12 02:40:00 +0300

    Added QR Scanner

--- App.js
+++ App.js
@@ -1,114 +1,119 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Platform, TouchableOpacity, PermissionsAndroid, ActivityIndicator } from 'react-native';
+import { CameraKitCameraScreen, } from 'react-native-camera-kit';
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      QR_Code_Value: '',
+      Start_Scanner: false,
+      Loading:false,
+    };
+  }
+  onQR_Code_Scan_Done = (QR_Code) => {
+    this.setState({ QR_Code_Value: QR_Code,Start_Scanner: false, Loading:true });
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        code: QR_Code
+      }),
+    }).then(res => res.json())
+        .then(response => {
+          console.log('Success:', JSON.stringify(response));
+          this.setState({Loading:false});
+        })
+        .catch(error => {
+          alert(error);
+          this.setState({Loading:false});
+        });
+  };
+  open_QR_Code_Scanner=()=> {
+    let that = this;
+    if (Platform.OS === 'android') {
+      async function requestCameraPermission() {
+        try {
+          const granted = await PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.CAMERA, {
+                'title': 'Camera App Permission',
+                'message': 'Camera App needs access to your camera '
+              }
+          );
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            that.setState({ QR_Code_Value: '', Start_Scanner: true });
+          } else {
+            alert("CAMERA permission denied");
+          }
+        } catch (err) {
+          alert("Camera permission err", err);
+          console.warn(err);
+        }
+      }
+      requestCameraPermission();
+    } else {
+      that.setState({ QR_Code_Value: '', Start_Scanner: true});
+    }
+  };
+  render() {
+    if (!this.state.Start_Scanner) {
+      return (
+          <View style={styles.MainContainer}>
+            <Text style={{ fontSize: 22, textAlign: 'center' }}>DEVFEST QR Kod Okuyucu</Text>
+            {this.state.Loading?
+                <ActivityIndicator size="large" color="#0000ff" />:
+                <View>
+                  <Text style={styles.QR_text}>
+                    {this.state.QR_Code_Value ? 'Scanned QR Code: ' + this.state.QR_Code_Value : ''}
+                  </Text>
+                  <TouchableOpacity
+                      onPress={this.open_QR_Code_Scanner}
+                      style={styles.button}>
+                    <Text style={{ color: '#FFF', fontSize: 14 }}>
+                      Tara
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+            }
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
-
+      );
+    }
+    return (
+        <View style={{ flex: 1 }}>
+          <CameraKitCameraScreen
+              showFrame={true}
+              scanBarcode={true}
+              laserColor={'#FF3D00'}
+              frameColor={'#00C853'}
+              colorForScannerFrame={'black'}
+              onReadCode={event =>
+                  this.onQR_Code_Scan_Done(event.nativeEvent.codeStringValue)
+              }
+          />
+        </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  MainContainer: {
+    flex: 1,
+    paddingTop: (Platform.OS) === 'ios' ? 20 : 0,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  QR_text: {
+    color: '#000',
+    fontSize: 19,
+    padding: 8,
+    marginTop: 12
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  button: {
+    backgroundColor: '#2979FF',
+    alignItems: 'center',
+    padding: 12,
+    width: 300,
+    marginTop: 14
   },
 });
-
-export default App;
